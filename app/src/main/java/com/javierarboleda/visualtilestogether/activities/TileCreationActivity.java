534057fe6fb03644070a36be125ef0bb255d63e3
Javier Arboleda <javier.g.arboleda@gmail.com>
2016-11-12 15:50:08 -0800

    feat: Add upload bitmap to Tiles table functionality

--- app/src/main/java/com/javierarboleda/visualtilestogether/activities/TileCreationActivity.java
+++ app/src/main/java/com/javierarboleda/visualtilestogether/activities/TileCreationActivity.java
@@ -1,21 +1,42 @@
 package com.javierarboleda.visualtilestogether.activities;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.javierarboleda.visualtilestogether.R;
 import com.javierarboleda.visualtilestogether.databinding.ActivityTileCreationBinding;
+import com.javierarboleda.visualtilestogether.fragments.ChannelAddDialog;
+import com.javierarboleda.visualtilestogether.fragments.ShapeAddDialog;
+import com.javierarboleda.visualtilestogether.models.Channel;
+import com.javierarboleda.visualtilestogether.models.Tile;
 import com.javierarboleda.visualtilestogether.views.CanvasView;
 
-public class TileCreationActivity extends AppCompatActivity {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+
+public class TileCreationActivity extends AppCompatActivity
+        implements ChannelAddDialog.OnFragmentInteractionListener,
+        ShapeAddDialog.OnFragmentInteractionListener {
 
     ActivityTileCreationBinding binding;
     CanvasView mCanvas;
+    private static final String TILES_TABLE = "tiles";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +93,9 @@ public class TileCreationActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.action_add_tile_to_upcoming:
+                Bitmap bitmap = mCanvas.getScaleBitmap(300, 300);
+                onFragmentInteraction(bitmap);
+                finish();
                 return true;
             case R.id.action_undo:
                 mCanvas.undo();
@@ -103,12 +127,40 @@ public class TileCreationActivity extends AppCompatActivity {
                 return true;
         }
 
-
         return super.onOptionsItemSelected(item);
     }
 
-    // Need to create square view to create artwork
+    @Override
+    public void onFragmentInteraction(Channel channel) {
 
+    }
 
+    @Override
+    public void onFragmentInteraction(Bitmap bitmap) {
+        // get the shapes folder of Firebase Storage for this app
+        FirebaseStorage mFirebaseStorage = FirebaseStorage.getInstance();
+        final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child(TILES_TABLE);
+        final String key = dbRef.push().getKey();
 
+        StorageReference shapesRef = mFirebaseStorage
+                .getReferenceFromUrl("gs://visual-tiles-together.appspot.com")
+                .child("shapes");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        ByteArrayInputStream inputstream = new ByteArrayInputStream(baos .toByteArray());
+        UploadTask uploadTask = shapesRef.child(key).putStream(inputstream);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dbRef.child(key).setValue(null);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Tile tile = new Tile(false, 0, 0, null, downloadUrl.toString(), new Date());
+                dbRef.child(key).setValue(tile);
+            }
+        });
+    }
 }
