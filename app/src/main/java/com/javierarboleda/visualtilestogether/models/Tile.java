0b4fff7111587f0b61e731fb317801c0e174097b
geocohn <geo@creationgroundmedia.com>
2016-11-20 11:28:39 -0800

    feat: Limit each user's tile vote to yes, no, or neutral

--- app/src/main/java/com/javierarboleda/visualtilestogether/models/Tile.java
+++ app/src/main/java/com/javierarboleda/visualtilestogether/models/Tile.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class Tile {
     public static final String TABLE_NAME = "tiles";
     public static final String CHANNEL_ID = "channelId";
-    public static final String POS_VOTES_ID = "posVotes";
+    public static final String USER_VOTES = "userVotes";
     private String shapeUrl;
     private String shapeFbStorage;
     private String creatorId;
